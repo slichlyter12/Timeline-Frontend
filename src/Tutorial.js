3a4c019b242518b9f48a9210ded3b7c0d384722f
Sam Lichlyter <slichlyter12@me.com>
2018-11-13 15:53:38 -0800

    converted markdown to html

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,35 +1,31 @@
 import React from 'react';
-const marked = require('marked');
-
-const markdown = `
-# How to export your data from [Google Timeline](https://www.google.com/maps/timeline)
-
-1. [Click Here](https://takeout.google.com/settings/takeout/custom/location_history) to take you to Google Takeout.
-2. Sign in if you aren't already
-3. Click "Next"
-4. Click "Create Archive"
-5. Grab a cup of coffee while your data exports...
-6. Once the page reloads and your archive is available, click the "Download" button
-7. Enter your password
-8. Celebrate because now you have all your location data!
-9. Continue below to send us your data.
-
-# How to send us your location data
-1. [Click Here](#) to go to our upload page
-2. Select your location archive from your downloads folder
-3. Click "Upload"
-4. Celebrate because now you're done!! 
-`;
 
 class Tutorial extends React.PureComponent {
-	getTutorial() {
-		let html = marked(markdown);
-		return { __html: html };
-	}
 
 	render() {
 		return (
-			<div className="Tutorial" dangerouslySetInnerHTML={this.getTutorial()}></div>
+			<div className="Tutorial">
+				<h1>How to export your data from <a href="https://www.google.com/maps/timeline">Google Timeline</a></h1>
+				<ol>
+					<li><a href="https://takeout.google.com/settings/takeout/custom/location_history">Click here</a> to take you to Google Takeout</li>
+					<li>Sign in if you aren't already</li>
+					<li>Click "Next"</li>
+					<li>Click "Create Archive"</li>
+					<li>Grab a cup of coffee while your data exports...</li>
+					<li>Once the page reloads and your archive is availabe, click the "Download" button</li>
+					<li>Enter your password</li>
+					<li>Celebrate because now you have all your location data!</li>
+					<li>Continue below to send us your data.</li>
+				</ol>
+
+				<h1>How to send us your location data</h1>
+				<ol>
+					<li><a href="/upload" target="iframe">Click here</a> to go to our upload page</li>
+					<li>Select your location archive from your downloads folder</li>
+					<li>Click "Upload"</li>
+					<li>Celebrate because now you're done!!</li>
+				</ol>
+			</div>
 		);
 	}
 }
